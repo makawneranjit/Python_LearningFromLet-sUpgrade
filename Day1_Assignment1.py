bf486155f07ff0066f18aa05903add2d5d0ca62c
makawneranjit <68535617+makawneranjit@users.noreply.github.com>
2020-07-20 13:37:03 +0530

    Create Day1_Assignment1.py

---
+++ Day1_Assignment1.py
@@ -0,0 +1,18 @@
+1. What is python? What are the ways to install python?
+
+Python is an interpreteed, object-oriented, high level programming language with dynamic semantics.
+Python is typically involves less amount of code and its syntax is easier to read.
+
+Ways to install Python:
+  1. Anaconda
+  2. Pycharm
+  3. Download Python3
+  
+ 
+ 2. What are the code environments and its types?
+ 
+ Code environment is a place where we can write, edit, compile and see output easily.
+ Types:
+      1. Local Environment
+      2. Virtual Environment
+      3. Cloud Environment
